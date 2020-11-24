485c402a95ac704e4d78678a66447b733c0bee45
k4deng <70725766+k4deng@users.noreply.github.com>
2020-11-24 12:38:16 -0500

    Add files via upload

---
+++ interests/minecraft.php
@@ -0,0 +1,102 @@
+<?php
+    include('../includes/header1.php');
+?>
+	 
+	 <title>K4denG | Minecraft</title>
+	 <meta name="title" content="K4denG | Minecraft">
+     <meta name="description" content="I like to play Minecraft a lot. You may have found me after playing one of my maps, or by playing on a server with me.">
+	 <meta name="keywords" content="k4deng, minecraft">
+
+<?php
+    include('../includes/header2.php');
+?>
+    <!-- ***** Main Banner Area Start ***** -->
+    <div class="main-banner header-text" id="top">
+        <div class="Modern-Slider">
+          <!-- Item -->
+          <div class="item">
+            <div class="img-fill">
+                <img src="/assets/images/slide-01.jpg" alt="">
+                <div class="text-content">
+                  <h3>Minecraft</h3>
+                  <h5>I like to play minecraft a lot. See more on this page!</h5>
+                  <!--<a href="#contact-us" class="main-stroked-button">Contact</a>-->
+                  <!--<a href="#about" class="main-filled-button">About Me</a>-->
+                </div>
+            </div>
+          </div>
+          <!-- // Item -->
+        </div>
+    </div>
+    <div class="scroll-down scroll-to-section"><a href="#about"><i class="fa fa-arrow-down"></i></a></div>
+    <!-- ***** Main Banner Area End ***** -->
+
+    <!-- ***** About Area Starts ***** -->
+    <section class="section" id="about">
+        <div class="container">
+            <div class="row">
+			
+                <div id="summary" class="col-lg-12 col-md-12 col-xs-12">
+                    <div class="left-text-content">
+                        <div class="section-heading">
+                            <h6>Minecraft</h6>
+                            <h2>Summary</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-8 col-xs-6">
+                    <div class="right-text-content">
+                        <p>
+							Hello! In case you didn't know, I play minecraft a lot. I make maps and play on servers with friends. 
+                        <br><br>
+							You can find me playing on mostly the same servers so, it's not that hard to find me. Downloads to my maps can also be found on this page.
+						</p>
+                    </div>
+                </div>
+				
+				<div id="maps"class="col-lg-12 col-md-12 col-xs-12">
+                    <div class="left-text-content">
+                        <div class="section-heading">
+                            <h2> </h2>
+                            <h2>Maps</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-8 col-xs-6">
+                    <div class="right-text-content">
+                        <p>
+							Currently, I have released none of my maps to the public. You may ask to beta test on of my maps by <a href="/home/#contact-us">contacting me</a>.
+						</p>
+                    </div>
+                </div>
+				
+				<div id="servers" class="col-lg-12 col-md-12 col-xs-12">
+                    <div class="left-text-content">
+                        <div class="section-heading">
+                            <h2> </h2>
+                            <h2>Servers</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-8 col-xs-6">
+                    <div id="earthvision" class="right-text-content">
+                        <p>
+							Want to know what servers I play on? A full list is here. <br> <i>This list may not be up to date. Last updated on 9/27/2020.</i>
+						<br><br>
+							<b>1. EarthVision</b>
+						<br>
+							EarthVision is a cool and exiting Earth server with real crossplay and a ton of features. EarthVision is a Java AND Minecraft: Bedrock Edition earth server!
+						<br>
+							<i>You can find their official website at <a href="https://earthvision.tk" target="_blank">earthvision.tk</a>.</i>
+						</p>
+                    </div>
+                </div>
+				
+            </div>
+        </div>
+    </section>
+    <!-- ***** About Area Ends ***** -->
+
+<?php
+    include('../includes/footer.php');
+?>
