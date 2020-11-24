c77899303c3849750f10e808d4cc47037c2a21bc
k4deng <70725766+k4deng@users.noreply.github.com>
2020-11-24 12:38:37 -0500

    Add files via upload

---
+++ interests/template.php
@@ -0,0 +1,82 @@
+<?php
+    include('../includes/header1.php');
+?>
+	 
+	 <title>K4denG | Template</title>
+	 <meta name="title" content="K4denG | Template">
+     <meta name="description" content="This is a template page for items on k4deng.ml.">
+	 <meta name="keywords" content="">
+	 <meta name="robots" content="noindex">
+
+<?php
+    include('../includes/header2.php');
+?>
+
+    <!-- ***** Main Banner Area Start ***** -->
+    <div class="main-banner header-text" id="top">
+        <div class="Modern-Slider">
+          <!-- Item -->
+          <div class="item">
+            <div class="img-fill">
+                <img src="/assets/images/slide-01.jpg" alt="">
+                <div class="text-content">
+                  <h3>Title</h3>
+                  <h5>Description Longer</h5>
+                  <!--<a href="#contact-us" class="main-stroked-button">Contact</a>-->
+                  <!--<a href="#about" class="main-filled-button">About Me</a>-->
+                </div>
+            </div>
+          </div>
+          <!-- // Item -->
+        </div>
+    </div>
+    <div class="scroll-down scroll-to-section"><a href="#about"><i class="fa fa-arrow-down"></i></a></div>
+    <!-- ***** Main Banner Area End ***** -->
+
+    <!-- ***** About Area Starts ***** -->
+    <section class="section" id="about">
+        <div class="container">
+            <div class="row">
+			
+                <div class="col-lg-12 col-md-12 col-xs-12">
+                    <div class="left-text-content">
+                        <div class="section-heading">
+                            <h6>Sub-Header</h6>
+                            <h2>Header</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-8 col-xs-6">
+                    <div class="right-text-content">
+                        <p>
+							Text line 1.
+                        <br><br>
+							Text line 2.
+						</p>
+                    </div>
+                </div>
+				
+				<div class="col-lg-12 col-md-12 col-xs-12">
+                    <div class="left-text-content">
+                        <div class="section-heading">
+                            <h2> </h2>
+                            <h2>Header</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-8 col-xs-6">
+                    <div class="right-text-content">
+                        <p>
+							Text line 1.
+						</p>
+                    </div>
+                </div>
+				
+            </div>
+        </div>
+    </section>
+    <!-- ***** About Area Ends ***** -->
+
+<?php
+    include('../includes/footer.php');
+?>
---
+++ interests/web-dev.php
@@ -0,0 +1,67 @@
+<?php
+    include('../includes/header1.php');
+?>
+	 
+	 <title>K4denG | Web Develepment</title>
+	 <meta name="title" content="K4denG | Web Develepment">
+     <meta name="description" content="In school, I have taken coding classes and have extensive knowledge of web development. I made the website you're on myself!">
+	 <meta name="keywords" content="k4deng, Web Develepment">
+
+<?php
+    include('../includes/header2.php');
+?>
+
+    <!-- ***** Main Banner Area Start ***** -->
+    <div class="main-banner header-text" id="top">
+        <div class="Modern-Slider">
+          <!-- Item -->
+          <div class="item">
+            <div class="img-fill">
+                <img src="/assets/images/slide-01.jpg" alt="">
+                <div class="text-content">
+                  <h3>Web Develepment</h3>
+                  <h5>In school, I have taken coding classes and have extensive knowledge of web development. I made the website you're on myself!</h5>
+                  <!--<a href="#contact-us" class="main-stroked-button">Contact</a>-->
+                  <!--<a href="#about" class="main-filled-button">About Me</a>-->
+                </div>
+            </div>
+          </div>
+          <!-- // Item -->
+        </div>
+    </div>
+    <div class="scroll-down scroll-to-section"><a href="#about"><i class="fa fa-arrow-down"></i></a></div>
+    <!-- ***** Main Banner Area End ***** -->
+
+    <!-- ***** About Area Starts ***** -->
+    <section class="section" id="about">
+        <div class="container">
+            <div class="row">
+			
+                <div id="summary" class="col-lg-12 col-md-12 col-xs-12">
+                    <div class="left-text-content">
+                        <div class="section-heading">
+                            <h6>Web Develepment</h6>
+                            <h2>Summary</h2>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 col-md-8 col-xs-6">
+                    <div class="right-text-content">
+                        <p>
+							In school, I have taken coding classes and have extensive knowledge of web development.
+                        <br><br>
+							I have made a few websites for people and the website you're on right now was coded by me!
+						<br><br>
+							If you want a website made for you, contact me and I may make a website for you free of charge.
+						</p>
+                    </div>
+                </div>
+				
+            </div>
+        </div>
+    </section>
+    <!-- ***** About Area Ends ***** -->
+
+<?php
+    include('../includes/footer.php');
+?>
