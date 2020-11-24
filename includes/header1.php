c5c83819e776ee5a75730524da203ea5a250ad3d
k4deng <70725766+k4deng@users.noreply.github.com>
2020-11-24 12:35:20 -0500

    Add files via upload

---
+++ includes/header1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	
+	 <!-- Global site tag (gtag.js) - Google Analytics -->
+	 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-175836514-1"></script>
+	 <script>
+	   window.dataLayer = window.dataLayer || [];
+	   function gtag(){dataLayer.push(arguments);}
+	   gtag('js', new Date());
+ 
+	   gtag('config', 'UA-175836514-1');
+	 </script>
+
+	 <!-- META -->
+	 <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
